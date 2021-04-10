0901dbe0492f7b75ba4bd01a6b2b802e3a0bf415
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-04-10 19:17:22 +0530

    🛅 next-firebase-auth Init

---
+++ utils/firebase/auth/initAuth.js
@@ -0,0 +1,49 @@
+import { init } from "next-firebase-auth";
+
+const initAuth = () => {
+  init({
+    authPageURL: "/login",
+    appPageURL: "/dashboard",
+    loginAPIEndpoint: "/api/login", // required
+    logoutAPIEndpoint: "/api/logout", // required
+    // Required in most cases.
+    firebaseAdminInitConfig: {
+      credential: {
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        // The private key must not be accesssible on the client side.
+        privateKey: process.env.FIREBASE_PRIVATE_KEY
+          ? JSON.parse(process.env.FIREBASE_PRIVATE_KEY)
+          : undefined,
+      },
+      databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    },
+    firebaseClientInitConfig: {
+      apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    },
+    cookies: {
+      name: "NextFirebase", // required
+      // Keys are required unless you set `signed` to `false`.
+      // The keys cannot be accessible on the client side.
+      keys: [
+        process.env.COOKIE_SECRET_CURRENT,
+        process.env.COOKIE_SECRET_PREVIOUS,
+      ],
+      httpOnly: false,
+      maxAge: 12 * 60 * 60 * 24 * 1000, // twelve days
+      overwrite: true,
+      path: "/",
+      sameSite: "strict",
+      secure: process.env.COOKIE_SECURE, // set this to false in local (non-HTTPS) development
+      signed: true,
+    },
+  });
+};
+
+export default initAuth;
