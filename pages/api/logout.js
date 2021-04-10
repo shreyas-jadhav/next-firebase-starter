06278fa861c15e34071a535c145ce9da15258425
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-04-10 19:14:24 +0530

    🔐next-firebase-auth API

---
+++ pages/api/logout.js
@@ -0,0 +1,15 @@
+import { unsetAuthCookies } from "next-firebase-auth";
+import initAuth from "../../utils/firebase/auth/initAuth";
+
+initAuth();
+
+const handler = async (req, res) => {
+  try {
+    await unsetAuthCookies(req, res);
+  } catch (e) {
+    return res.status(500).json({ error: "Unexpected error." });
+  }
+  return res.status(200).json({ success: true });
+};
+
+export default handler;
