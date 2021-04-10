635de919eea9665efd0f2c5eaf571de6d7bd2974
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-04-10 19:11:12 +0530

    🔃 Loader component

---
+++ components/elements/Loader.js
@@ -0,0 +1,11 @@
+const Loader = () => {
+  return (
+    <div className="container mx-auto my-20">
+      <h1 className="text-6xl text-blue text-center font-extrabold">
+        Loading...
+      </h1>
+    </div>
+  );
+};
+
+export default Loader;
