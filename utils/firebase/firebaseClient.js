9e5723de6d39e40a0e1cf00193d6d73ad058827b
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-04-10 19:16:50 +0530

    🔥 Firebase Client SDK

---
+++ utils/firebase/firebaseClient.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+
+const clientCredentials = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(clientCredentials);
+}
+
+export default firebase;
