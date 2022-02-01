172d035817f855563c2539d97f2e3df171589d8e
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2022-02-01 09:52:39 +0530

    👨‍🔧 update dependencies

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  future: {
-    webpack5: false,
-  },
+  // Webpack 5 is enabled by default
+  // You can still use webpack 4 while upgrading to the latest version of Next.js by adding the "webpack5: false" flag
+  webpack5: false,
 };
